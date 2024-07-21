76820da184688dc9db2f0079b3e65a4838faf68f
hosseinshafie6152 <139880697+hosseinshafie6152@users.noreply.github.com>
2024-07-21 13:08:48 +0430

    fixed and delete tabbar state and added <outlet/> in the last document .

--- src/pages/Accout.Component.jsx
+++ src/pages/Accout.Component.jsx
@@ -4,16 +4,13 @@ import IconBill from '../icons/IconBill'
 import IconOrders from '../icons/IconOrders'
 import IconProfile from '../icons/IconProfile'
 import IconSupport from '../icons/IconSupport'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function AccountPage() {
 
-  const [tabBar , setTabBar] = useState(<ProfileComponent/>)
-
-
   return (
     <div className="w-[1440px] bg-gray1 mx-auto border-t-2 border-t-gray2 my-10 flex justify-center">
-      {tabBar}
+      
       <div className=" w-[500px] bg-white flex flex-col justify-center items-center rounded-2xl m-7">
         <div id="specifications" className=" mb-7 flex flex-col justify-center items-center">
           <img src="./images/logo.png" alt="profile" className=" rounded-full w-40" />
@@ -29,7 +26,7 @@ function AccountPage() {
             <IconOrders/>
             my orders
           </Link>
-          <Link to='/support' id="support" className="bg-gray1 flex justify-center items-center rounded-md flex-col hover:font-bold hover:scale-105 " >
+          <Link to='/account/support' id="support" className="bg-gray1 flex justify-center items-center rounded-md flex-col hover:font-bold hover:scale-105 " >
             <IconSupport/>
             Support
           </Link>
@@ -39,7 +36,7 @@ function AccountPage() {
           </Link>
         </div>
       </div>
-
+      <Outlet/>
     </div>
   )
 }
