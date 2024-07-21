299cdd4d878675de3cdb94868ae7cd4433d59c61
hosseinshafie6152 <139880697+hosseinshafie6152@users.noreply.github.com>
2024-07-21 13:10:35 +0430

    create cart component whith components loading and listCard

--- src/pages/Cart.Component.jsx
+++ src/pages/Cart.Component.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import ListCard from "../components/card/ListCard";
+import useFetch from '../hooks/useFetch'
+import  IconBigBasket from "../icons/IconBigBasket";
+import Loading from '../components/Loading'
 
 function CartPage() {
-  return (
-    <div>CartPage</div>
-  )
+  const {data} = useFetch('http://localhost:8000/shopping')
+  while (data === undefined) {
+    return(
+      <><Loading/></>
+    )
+  }
+  if (data?.length === 0) {
+    return(
+      <div className=" bg-gray1 w-[1440px] h-screen mx-auto border-t-2 flex flex-col justify-center items-center">
+        <IconBigBasket/>
+        <p className=" font-bold text-l text-gray4 ">the cart is empty!</p>
+      </div>
+    )
+  }else{
+    return (
+      <div>
+        <ListCard products={data}/>
+      </div>
+    )
+  }
+
 }
 
 export default CartPage
