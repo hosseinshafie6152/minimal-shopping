c408852ead6d8e6e1a5ef36115f184b077429644
hosseinshafie6152 <139880697+hosseinshafie6152@users.noreply.github.com>
2024-07-21 13:26:25 +0430

    fixed . validation before to post with check the item there is in selected list or not

--- src/components/btns/BtnAddToCart.jsx
+++ src/components/btns/BtnAddToCart.jsx
@@ -1,43 +1,58 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Loading from '../Loading';
 import { useSelect } from '../../store/useSelect';
+import BtnDelete from './BtnDelete';
+import BtnEdit from './BtnEdit';
 
 function BtnAddToCart({ product, size, count }) {
+    const { setSelect } = useSelect((state) => state.action);
+    const { selectedList } = useSelect((state) => state);
     const [innerText, setInnerText] = useState('Add to cart');
-    const [className , setClassName] = useState('w-[127px] border border-orange text-orange font-medium rounded-[31.5px] mr-3');
-    const {setSelect} = useSelect((state)=>state.action);
-
+    const className = useRef('w-[127px] border border-orange text-orange font-medium rounded-[31.5px] mr-3');
+    const [btnDeleteEdit, setBtnDeltetEdit] = useState();
 
     function addCard() {
-        if (size.current === null) {
+        if (selectedList?.some((item) => item.id === product.id)) {
+            document.getElementById('post').remove()
             return (
-                document.getElementById('warning').innerText = 'You have not yet selected a size for your purchase!'
+                setBtnDeltetEdit(<>
+                    <BtnDelete id={product.id}/>
+                    <BtnEdit id={product.id} />
+                </>)
             )
         } else {
-            setInnerText(<Loading />);
-            setTimeout(() => {
-                fetch('http://localhost:8000/shopping', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        'id': product.id,
-                        'price': product.price,
-                        'srcImg': product.srcImage,
-                        'count': count,
-                        'size': size.current,
-                        'description': product.description
-                    })
-                }).then((response) => response.json().then((res) =>{ setInnerText('added to cart') ; setSelect(res)}))
-            }, 2000)
-            
+            if (size.current === null) {
+                return (
+                    document.getElementById('warning').innerText = 'You have not yet selected a size for your purchase!'
+                )
+            } else {
+                setInnerText(<Loading />);
+                setTimeout(() => {
+                    fetch('http://localhost:8000/shopping', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            'id': product.id,
+                            'price': product.price,
+                            'srcImg': product.srcImg,
+                            'count': count,
+                            'size': size.current,
+                            'description': product.description
+                        })
+                    }).then((response) => response.json().then((res) => { setInnerText('added to cart'); setSelect(res);className.current = 'w-[127px] bg-green border border-green text-white font-medium rounded-[31.5px] mr-3' }))
+                }, 2000)
+
+            }
         }
+
     }
 
     return (
         <>
-            <button id='post' className={className} onClick={() => { addCard()}}>{innerText}</button>
+            <button id='post' className={className.current} onClick={() => { addCard() }}>{innerText}</button>
+            {btnDeleteEdit}
         </>
     )
 }
