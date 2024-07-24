ab9d1b9b396f7138094c5539260d1fa61f6cd5d6
hosseinshafie6152 <139880697+hosseinshafie6152@users.noreply.github.com>
2024-07-24 10:05:44 +0430

    This component updates an item in the database but prevents malicious requests from being sent to the backend with validation.

--- src/components/btns/BtnEdit.jsx
+++ src/components/btns/BtnEdit.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import { useSize } from '../../store/useSize';
 
-function BtnEdit({id}) {
+function BtnEdit({ item , position }) {
+    const { size } = useSize((state) => state)
+    const {setSize} = useSize((state)=>state.action)
+    
+    
+    function editfn() {
+        if (position === 'Home') {
+            window.scroll({
+                top: 100,
+                behavior: 'smooth'
+            });
+            document.getElementById('CountSize').className = ' mt-6 p-5 border  border-dashed border-gray2 rounded-xl ';
+            document.getElementById('ping').className = "w-5 h-5 bg-pink rounded-full absolute right-40 animate-ping";
+            if (size !== null) {
+                setTimeout(() => {
+                    fetch(`http://localhost:8000/shopping/${item.id}`, {
+                        method: "PUT",
+                        body: JSON.stringify({
+                            'id': item.id,
+                            'price': item.price,
+                            'size': size,
+                            "description": item.description
+                        })
 
-    function editfn(){
-        setTimeout(() => {
-            fetch(`http://localhost:8000/shopping/${id}`, {
-                method : "PUT",
-                
-            }).then((response)=> response.json().then((res)=>console.log(res)))
-        }, 2000);
+                    }).then((response) => response.json().then((res) =>setSize(null) ))
+                }, 2000);
+            }
+        }if (position === 'Orders') {
+            
+        }
     }
+
     return (
         <>
-            <button onClick={()=> editfn()} className='w-16 bg-green text-white rounded-md mx-2'>Edit</button>
+            <button onClick={() => editfn()} className='w-16 bg-green text-white rounded-md mx-2'>Edit</button>
         </>
     )
 }
