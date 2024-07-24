b3e5480d3722352232cf23b208bc9dd2178dcf4a
hosseinshafie6152 <139880697+hosseinshafie6152@users.noreply.github.com>
2024-07-24 10:35:12 +0430

    fixed but change mechanism for get and seting name and phoneNumber for user

--- src/pages/Accout.Component.jsx
+++ src/pages/Accout.Component.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import ProfileComponent from '../components/Account.Component/ProfileComponent'
+import useFetch from '../hooks/useFetch'
 import IconBill from '../icons/IconBill'
 import IconOrders from '../icons/IconOrders'
 import IconProfile from '../icons/IconProfile'
 import IconSupport from '../icons/IconSupport'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom';
+
+
 
 function AccountPage() {
+  const {data} = useFetch('http://localhost:8000/user');    
 
   return (
     <div className="w-[1440px] bg-gray1 mx-auto border-t-2 border-t-gray2 my-10 flex justify-center">
@@ -14,10 +16,10 @@ function AccountPage() {
       <div className=" w-[500px] bg-white flex flex-col justify-center items-center rounded-2xl m-7">
         <div id="specifications" className=" mb-7 flex flex-col justify-center items-center">
           <img src="./images/logo.png" alt="profile" className=" rounded-full w-40" />
-          <p id="name">hossein</p>
-          <p id="phoneNumber">0319694541</p>
+          <p id="name"></p>
+          <p id="phoneNumber"></p>
         </div>
-        <div id="nav" className="w-[500px] bg-white grid grid-cols-2 grid-rows-2 gap-3 p-4 text-[20px]">
+        <div id="nav" className="w-[500px] bg-white grid grid-cols-2 grid-rows-2 gap-3 p-4 text-[20px] ">
           <Link to='/account/profile' id="profile" className="bg-gray1 flex justify-center items-center rounded-md flex-col hover:font-bold hover:scale-105 " >
             <IconProfile/>
             profile
